a46340ce74681e01a2079764b80d0bb7bf361c3f
emag <lanabe.lanabe@gmail.com>
2016-04-19 16:27:27 +0900

    Done 119/506

--- ng-book2/inventory_app/app.ts
+++ ng-book2/inventory_app/app.ts
@@ -11,15 +11,110 @@ class Product {
 }
 
 @Component({
+  selector: 'price-display',
+  inputs: ['price'],
+  template: `
+    <div class="price-display">\${{price}}</div>`
+})
+class PriceDisplay {
+  price:number;
+}
+
+@Component({
+  selector: 'product-department',
+  inputs: ['product'],
+  template: `
+    <div class="product-department">
+      <span *ngFor="#name of product.department; #i=index">
+        <a href="#">{{name}}</a>
+        <span>{{i < (product.department.length - 1) ? '>' : ''}}</span>
+      </span>
+    </div>`
+})
+class ProductDepartment {
+  product:Product;
+}
+
+@Component({
+  selector: 'product-image',
+  host: {'class': 'ui small image'},
+  inputs: ['product'],
+  template: `
+    <img class="product-image" [src]="product.imageUrl"/>`
+})
+class ProductImage {
+  product: Product;
+}
+
+@Component({
+  selector: 'product-row',
+  inputs: ['product'],
+  host: {'class': 'item'},
+  directives: [ProductImage, ProductDepartment, PriceDisplay],
+  template: `
+    <product-image [product]="product"></product-image>
+    <div class="content">
+      <div class="header">{{product.name}}</div>
+      <div class="meta">
+        <div class="product-sku">SKU #{{product.sku}}</div>
+      </div>
+      <div class="description">
+        <product-department [product]="product"></product-department>
+      </div>
+    </div>
+    <price-display [price]="product.price"></price-display>`
+})
+class ProductRow {
+  product:Product;
+}
+
+@Component({
+  selector: 'product-list',
+  directives: [ProductRow],
+  template: `
+    <div class="ui items">
+      <product-row
+        *ngFor="#myProduct of productList"
+        [product]="myProduct"
+        (click)="clicked(myProduct)"
+        [class.selected]="isSelected(myProduct)">
+      </product-row>
+    </div>`,
+  inputs: ['productList'],
+  outputs: ['onProductSelected'],
+})
+class ProductList {
+  productList:Product[];
+  onProductSelected:EventEmitter<Product>;
+  currentProduct:Product;
+
+  constructor() {
+    this.onProductSelected = new EventEmitter();
+  }
+
+  clicked(product:Product):void {
+    this.currentProduct = product;
+    this.onProductSelected.emit(product);
+  }
+
+  isSelected(product:Product):boolean {
+    if (!product || !this.currentProduct) {
+      return false;
+    }
+    return product.sku === this.currentProduct.sku;
+  }
+
+}
+
+@Component({
   selector: 'inventory-app',
+  directives: [ProductList],
   template: `
     <div class="inventory-app">
-      <ul>
-        <li *ngFor="#product of products">
-          <h1>{{product.name}}</h1>
-         <span>{{product.sku}}</span>
-        </li>
-      </ul>
+      <product-list
+        [productList]="products"
+        (onProductSelected)="productWasSelected($event)">
+      </product-list>
     </div>`
 })
 class InventoryApp {
