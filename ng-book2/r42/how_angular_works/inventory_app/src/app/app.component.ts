2ae1c8fa211af0c09faffab15ecff3621e4a76e3
emag <lanabe.lanabe@gmail.com>
2016-11-04 08:48:39 +0900

    Add Products instead of a sinble Product in AppComponent

--- ng-book2/r42/how_angular_works/inventory_app/src/app/app.component.ts
+++ ng-book2/r42/how_angular_works/inventory_app/src/app/app.component.ts
@@ -10,16 +10,32 @@ import {Product} from './product/product.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  product: Product;
+  products: Product[];
 
   constructor() {
-    this.product = new Product(
-      'NICEHAT',
-      'A Nice Black Hat',
-      '/resources/images/products/black-hat.jpg',
-      ['Men', 'Accessories', 'Hats'],
-      29.99
-    );
+    this.products = [
+      new Product(
+        'MYSHOES',
+        'Black Running Shoes',
+        '/resources/images/products/black-shoes.jpg',
+        ['Men', 'Shoes', 'Running Shoes'],
+        109.99
+      ),
+      new Product(
+        'NEATOJACKET',
+        'Blue Jacket',
+        '/resources/images/products/blue-jacket.jpg',
+        ['Women', 'Apparel', 'Jackets & Vests'],
+        29.99
+      ),
+      new Product(
+        'NICEHAT',
+        'A Nice Black Hat',
+        '/resources/images/products/black-hat.jpg',
+        ['Men', 'Accessories', 'Hats'],
+        29.99
+      )
+    ];
   }
 
 }
